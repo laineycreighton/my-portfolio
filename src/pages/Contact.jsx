43e365f9e6d6bdf4be56e8b70abd8ee61a12aef7
Lainey <dev.lainey@gmail.com>
2023-09-21 15:09:41 -0400

    add styling to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,43 +9,35 @@ import github from '../assets/images/github.svg';
 import linkedin from '../assets/images/linkedin.svg';
 import email from '../assets/images/email.svg';
 
-//----- EXPORTS -----//
-export default function Dev() {
+//Components
+import Menu from '../components/Menu';
+import Footer from '../components/Footer';
 
-    //Email
+//----- EXPORTS -----//
+export default function Home() {
+
     const emailAddress = 'dev.lainey@gmail.com';
 
     //Return
     return (
-        //---------- BODY ----------//
-        <div className='dev-page'>
+        <div className='contact-page'>
 
-            {/*-- Nav --*/}
-            <div className='back'>
-                <Link className='back-link' to="/">Back</Link>
+            {/* Menu */}
+            <Menu />
+
+            {/*-- Heading --*/}
+            <div className='contact-header'>
+                <h5>Let's work together</h5>
+                <a className='hello' href={emailAddress} target='_blank' >
+                    say hello
+                </a>
             </div>
 
-            {/*-- Main --*/}
-            <div className='dev'>
-
-                {/* Header */}
-                <header className='dev-header'>
-                    <h3>Meet the Developer</h3>
-                </header>
-
-                {/* About */}
-                <div className='about'>
-                    <p>Hello, I'm Lainey Creighton, a passionate developer with a current focus in JavaScript and React, where I'm honing my skills to create engaging and interactive web experiences. I'm particularly drawn to the art of web design, using CSS to craft visually appealing and user-friendly websites.</p>
-                    <p>Beyond the screen, I thrive in the great outdoors. When I'm not coding, you can often find me paddle boarding, rafting, camping, hiking, and exploring the beauty of nature. I believe that just like in coding, the best adventures are the ones where you embrace the journey.</p>
-                    <p className='thank-you'>Thank you for visiting my site, and feel free to connect with me for networking, collaboration, or to explore the exciting world of web development together.</p>
-                </div>
-
-                {/* Contact */}
-                <div className='contact'>
-                    <a href='https://github.com/laineycreighton' target='_blank'><img src={github} alt='github logo' /></a>
-                    <a href='https://www.linkedin.com/in/lainey-creighton/' target='_blank'><img src={linkedin} alt='linkedin logo' /></a>
-                    <a href={`mailto:${emailAddress}`} target='_blank'><img src={email} alt='email logo' /></a>
-                </div>
+            {/* Contact */}
+            <div className='contact-links'>
+                <img src={github} />
+                <img src={linkedin} />
+                <img src={email} />
             </div>
         </div>
     )
